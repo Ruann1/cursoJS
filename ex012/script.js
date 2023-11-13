cdda0383bb8b28d1154b0e7dd93967e05e4c8747
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2023-11-13 20:55:21 -0300

    exercício 12

--- ex012/script.js
+++ ex012/script.js
@@ -1,20 +1,4 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+function verif() {
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 0 && hora < 12) {
-        //Bom dia!
-        img.src = 'imagens/manha.png'
-        document.body.style.background = '#ebcca0'
-    } else if (hora >= 12 && hora <= 18){
-        //Boa tarde!
-        img.src = 'imagens/tarde.png'
-        document.body.style.background = '#dc8d4b'
-    } else {
-        //Boa noite!
-        img.src = 'imagens/noite.png'
-        document.body.style.background = '#11242a'
-    }
+    var ano = data.getFullYear
 }
