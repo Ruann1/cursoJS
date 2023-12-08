5b423056e1de50d09a6ab21b6886a7514217c898
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2023-12-07 21:04:00 -0300

    update

--- ex015/script.js
+++ ex015/script.js
@@ -2,15 +2,11 @@ function verif() {
     var tempo = new Date() //DATA
     var data = tempo.getFullYear() //Escolhemos o dado que queremos da variável acima
     
-    var ano = document.getElementById('iano')
-    var form = (ano.value)
-    var idade = data - form3
-    var img = document.createElement('img')
-    img.setAttribute('id', 'foto')
-
-    document.getElementById('res').innerHTML = `Você nasceu no ano de ${form} e tem ${idade} anos!`
-
-    img.setAttribute('src', 'adulto-m')
-
+    var nascimento = document.get('iano')
+    var ano = (data - nascimento)        
+    
+    var res = window.document.getElementById('res')
+    res.innerHTML = `Você nasceu no ano de ${nascimento} e sua idade é ${ano}`
+     
     
 }
