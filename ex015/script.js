89e4df34489c73165686e78ee23e1fa0759e07a3
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2023-12-08 17:42:15 -0300

    primeira parte concluida

--- ex015/script.js
+++ ex015/script.js
@@ -2,11 +2,11 @@ function verif() {
     var tempo = new Date() //DATA
     var data = tempo.getFullYear() //Escolhemos o dado que queremos da variável acima
     
-    var nascimento = document.get('iano')
-    var ano = (data - nascimento)        
+    var nascimento = window.document.getElementById('iano').value 
+    var ano = data - nascimento        
     
     var res = window.document.getElementById('res')
-    res.innerHTML = `Você nasceu no ano de ${nascimento} e sua idade é ${ano}`
-     
+    res.innerHTML = `Você nasceu em ${nascimento} e sua idade é ${ano}`
     
+
 }
