ee1817923a42a65382baf1a9d8e942992864f0e6
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2023-12-13 17:47:46 -0300

    a

--- ex015/script.js
+++ ex015/script.js
@@ -6,10 +6,13 @@ function verif() {
     var ano = data - nascimento        
     
     var res = window.document.getElementById('res')
+
+    var foto = window.document.getElementById('imagem')
     
     
     if (ano < 25) {
-        res.innerHTML = `Você nasceu em ${nascimento} e sua idade é ${ano}`
+        res.innerHTML = `Você nasceu em ${nascimento} e é um jovem de ${ano} anos`
+        foto.src = 'jovem-m.png'
     }
 
 }
