89f73f8a6423ff14fd134015a1e4a22a283f75ef
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-01-16 17:20:04 -0300

    Repetições

---
+++ aula 13/ambiente.js
@@ -0,0 +1,12 @@
+var c = 1
+
+//while (c <= 6){  //condição
+//    console.log('Você conseguiu!') //texto
+//    c++ // c = c + 1, operação
+//}
+
+
+do{ //Outra maneira de utilizar as repetições 
+    console.log('Tudo Bom')
+    c++
+} while (c <= 6)
