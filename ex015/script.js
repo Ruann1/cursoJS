bc31d4140dfedaad506abbd0c2aecf7a31c81bdc
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-01-16 14:39:09 -0300

    .

--- ex015/script.js
+++ ex015/script.js
@@ -10,38 +10,37 @@ function verif() {
 
     var foto = window.document.getElementById('imagem')
     
-    if (genero === 'mas') {
-        if (ano < 5) {
-            res.innerHTML = `Você nasceu em ${nascimento} e é um    bebê de ${ano} anos`
-            foto.src = 'pessoas/bebe-m.png'
-        } else if (ano < 25) {
-            res.innerHTML = `Você nasceu em ${nascimento} e é um    jovem de ${ano} anos`
-            foto.src = 'pessoas/jovem-m.png'
-        }
+
+    if (ano < 5) {
+        res.innerHTML = `Você nasceu em ${nascimento} e é um    bebê de ${ano} anos`
+        foto.src = 'pessoas/bebe-m.png'
+    } else if (ano < 25) {
+        res.innerHTML = `Você nasceu em ${nascimento} e é um    jovem de ${ano} anos`
+        foto.src = 'pessoas/jovem-m.png'
+    }
         else if (ano < 65) {
-            res.innerHTML = `Você nasceu em ${nascimento} e é um    adulto de ${ano} anos`
-            foto.src = 'pessoas/adulto-m.png'
-        } 
-        else {
-            res.innerHTML = `Você nasceu em ${nascimento} e é um    idoso de ${ano} anos`
-            foto.src = 'pessoas/idoso-m.png'
-        }
-    } else if (genero === fem) {
-        if (ano < 5) {
-            res.innerHTML = `Você nasceu em ${nascimento} e é um    bebê de ${ano} anos`
-            foto.src = 'pessoas/bebe-f.png'
-        } else if (ano < 25) {
-             res.innerHTML = `Você nasceu em ${nascimento} e é um    jovem de ${ano} anos`
-             foto.src = 'pessoas/jovem-f.png'
-        }
-        else if (ano < 65) {
-            res.innerHTML = `Você nasceu em ${nascimento} e é um    adulto de ${ano} anos`
-            foto.src = 'pessoas/adulto-f.png'
-        } 
-        else {
-            res.innerHTML = `Você nasceu em ${nascimento} e é um    idoso de ${ano} anos`
-            foto.src = 'pessoas/idosa-f.png'
-        }
+        res.innerHTML = `Você nasceu em ${nascimento} e é um    adulto de ${ano} anos`
+        foto.src = 'pessoas/adulto-m.png'
+    } 
+    else {
+        res.innerHTML = `Você nasceu em ${nascimento} e é um    idoso de ${ano} anos`
+        foto.src = 'pessoas/idoso-m.png'
+    }
+    
+    if (ano < 5) {
+        res.innerHTML = `Você nasceu em ${nascimento} e é um    bebê de ${ano} anos`
+        foto.src = 'pessoas/bebe-f.png'
+    } else if (ano < 25) {
+         res.innerHTML = `Você nasceu em ${nascimento} e é um    jovem de ${ano} anos`
+         foto.src = 'pessoas/jovem-f.png'
+    }
+    else if (ano < 65) {
+        res.innerHTML = `Você nasceu em ${nascimento} e é um    adulto de ${ano} anos`
+        foto.src = 'pessoas/adulto-f.png'
+    } 
+    else {
+        res.innerHTML = `Você nasceu em ${nascimento} e é um    idoso de ${ano} anos`
+        foto.src = 'pessoas/idosa-f.png'
     }
 
 }
