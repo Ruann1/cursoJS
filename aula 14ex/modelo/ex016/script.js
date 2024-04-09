76cab994faa9973793c4d7561b7f1c4aa3a5b123
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-04-09 18:53:46 -0300

    atualização exercicios

---
+++ aula 14ex/modelo/ex016/script.js
@@ -0,0 +1,9 @@
+let inicio = window.document.getElementById('istart')
+let ifim = window.document.getElementById('ifim')
+let ipasso = window.document.getElementById('ipasso')
+
+let start = Number(inicio.value)
+let final = Number(ifim.value)
+let passo = Number(ipasso.value)
+
+console.log('start');
---
+++ ex016/script.js
@@ -0,0 +1,9 @@
+let inicio = document.getElementById('istart')
+let ifim = document.getElementById('ifim')
+let ipasso = document.getElementById('ipasso')
+
+let start = Number(inicio.value)
+let final = Number(ifim.value)
+let passo = Number(ipasso.value)
+
+console.log(start);
