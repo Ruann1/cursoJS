18bc4e59ab41d9df2ea62b3dce63e04298132502
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-04-09 21:45:18 -0300

    att

--- aula 14ex/modelo/ex016/script.js
+++ aula 14ex/modelo/ex016/script.js
@@ -1,11 +1,16 @@
 let start = 1
 let final = 10
-let passo = 1
+let passo = 2
 let total = start + passo
 
 
 do{
-    start + passo
-}while(start <= final);
+    console.log(start)
+    start += passo - 1
+}while(start < final);
 
-console.log(total)
+    if(final != 10){
+        console.log(final)
+    } else{
+        console.log(final)
+    }
