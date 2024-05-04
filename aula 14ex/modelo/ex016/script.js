30a863640da6917dccc82ffd2e228c2396d5523c
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-05-03 21:53:12 -0300

    finalização de exercicio

--- aula 14ex/modelo/ex016/script.js
+++ aula 14ex/modelo/ex016/script.js
@@ -4,27 +4,40 @@ let inicio = document.getElementById('istart')
 let fim = document.getElementById('ifim')
 let passo = document.getElementById('ipasso')
 let conta = document.getElementById('iconta')
+b
 
-let s = Number(inicio.value)
-let f = Number(fim.value)
-let p = Number(passo.value)
+if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    conta.innerHTML = 'Impossível contar, Faltam dados!'
+}else{
+    conta.innerHTML = 'Contando.. <br><br>'
+    let s = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
 
-for(let c = s; c <= f; c += p){
-
-    conta.innerHTML += `${c}`
-
+    if(p <= 0){
+        conta.innerHTML = 'Impossível contar passo 0, alterando para 1!<br><br>'
+        p = 1
+    }
+    if(s < f){
+        //Contagem Crescente
+        for(let c = s; c <= f; c += p)
+        conta.innerHTML += ` ${c} `
+    }else{
+        //Contagem Decrescente 
+        for(let c = s; c >= f; c -= p)
+        conta.innerHTML += ` ${c} `
+    }
 }
 
-//do{
-//    let c = s
-//    c += p
 
-//}while(c <= f)
 
-//while(c <= f){
-//    let c = s
-//    c += p
-//    conta.innerHTML += `${c}`
-//}
+
+
+    
+
+
+
+
+
 
 }
