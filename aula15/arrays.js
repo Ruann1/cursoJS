ec805183249f35dc736732986fb4488f7013d461
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-05-10 13:57:43 -0300

    mudanças no código

--- aula15/arrays.js
+++ aula15/arrays.js
@@ -2,4 +2,4 @@ let posicao = [3, 6, 3, 5 ,8]
 
 for(let i = 0; i < posicao.length; i++){
     console.log(`A posição ${i} tem o valor ${posicao[i]}`)
-}
+}                                                                                                                            
