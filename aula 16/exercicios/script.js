c3576215616f9174efd621e341cf2f44157d0889
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-06-03 18:57:57 -0300

    att

---
+++ aula 16/exercicios/script.js
@@ -0,0 +1,20 @@
+function analise() {
+
+let num = document.getElementById('inum')
+let c = Number(num.value)
+let arr = []
+
+if(c == '' || c < 1 || c > 100){
+    window.alert('Campo obrigatório invalido!')
+}else{
+    for(let i; i = c.length; i++){
+
+        
+
+    }
+}
+
+
+
+
+}
