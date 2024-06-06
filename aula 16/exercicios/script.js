27b143dbae9af6c6c56d993027a100410486e727
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-06-06 16:34:01 -0300

    atualização no exercicio

--- aula 16/exercicios/script.js
+++ aula 16/exercicios/script.js
@@ -1,20 +1,22 @@
+caixa.innerHTML = ''
+
 function analise() {
 
 let num = document.getElementById('inum')
 let c = Number(num.value)
 let arr = []
 
+
 if(c == '' || c < 1 || c > 100){
     window.alert('Campo obrigatório invalido!')
 }else{
-    for(let i; i = c.length; i++){
 
-        
+    
+    caixa.innerHTML += `O valor ${c} foi adicionado... <br>` 
 
-    }
 }
 
-
+    cons.innerHTML += ` ${arr}`
 
 
 }
