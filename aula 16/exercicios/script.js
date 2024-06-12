37e19abd72b2cdb28831a9cb1254dd5539bc7ea8
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-06-12 20:57:18 -0300

    att

--- aula 16/exercicios/script.js
+++ aula 16/exercicios/script.js
@@ -1,22 +1,37 @@
+//In JavaScript, arrays use numbered indexes.  
+
+//In JavaScript, objects use named indexes.
+
 caixa.innerHTML = ''
 
 function analise() {
 
 let num = document.getElementById('inum')
 let c = Number(num.value)
-let arr = []
+let arr = [];
+
 
 
 if(c == '' || c < 1 || c > 100){
     window.alert('Campo obrigatório invalido!')
 }else{
 
-    
-    caixa.innerHTML += `O valor ${c} foi adicionado... <br>` 
+    for(let i = 0; i < arr.length; i++){
+        
+        arr.push(c)
+        caixa.innerHTML += `O valor ${c} foi adicionado... <br>` 
 
+    }
+
+
+
+    
+    
 }
 
-    cons.innerHTML += ` ${arr}`
+    cons.innerHTML += `${arr[i]}`
+
+    
 
 
 }
