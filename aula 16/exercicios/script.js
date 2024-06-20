d15ac62dfa9a3a4625bbaad5f38c7d851e6b41c8
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-06-20 00:13:45 -0300

    novo projeto analisador de númeiros

---
+++ aula 16/exercicios/script.js
@@ -0,0 +1,31 @@
+let num = document.getElementById('inum')
+let numero = Number(num.value)
+let lista = document.getElementById('icaixa')
+let arr = []
+
+function num(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function lista(l, n){
+    if(l.indexOf(n)){
+        return true
+    }else{
+        return false
+    }
+}
+
+
+
+function analise(){
+
+
+    if(num(numero.value) && lista(numero.value, arr)){
+        window.alert('Insira um número válido')
+    }
+
+}
