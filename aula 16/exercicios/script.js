d59ea22ddcc4afcdc64f1651a9c35169264f0b16
Ruan Gabriel <101756709+Ruann1@users.noreply.github.com>
2024-06-28 22:02:39 -0300

    Atualizações no analisador

--- aula 16/exercicios/script.js
+++ aula 16/exercicios/script.js
@@ -10,7 +10,7 @@ function isNumero(n){
     }
 }
 
-function lista(n, l){
+function inlista(n, l){
     if(l.indexOf(Number(n)) != -1){
         return true
     }else{
@@ -21,13 +21,32 @@ function lista(n, l){
 function analise(){
 
 
-    if(isNumero(num.value) && !lista(num.value, arr)){
+    if(isNumero(num.value) && !inlista(num.value, arr)){// o "!" antes da lista está verificando que o número informado NÃO está na lista.
         arr.push(Number(num.value)) // aqui puxa o valor do formulário para o array
         let item = document.createElement('option') // aqui criamos um option para adicionarmos nosso valor na lista 
         item.text = `Valor ${num.value} foi adicionado` //aqui o lado do usuário recebe a mensagem que o número que ele analisou foi adicionado
-        lista.appendChild(item) //aqui utilizamos essa tag para adicionar um elemento no caso um número ao FINAL da lista/arrayhc
+        lista.appendChild(item) //aqui utilizamos essa tag para adicionar um elemento no caso um número ao FINAL da lista/array
+
+        
     }else{
-        window.alert('Teste')
+        window.alert('Digite um valor válido')
     }
+}
+
+function finalizar(){
+
+    /* 
+    Ao todo temos X números cadastrados;
+    O maior valor informado foi X;
+    O menor valor informado foi X;
+    Somando todos os valores, temos X;
+    A média dos valores digitados é X.
+    */
+
+    //Esses são os dados que iremos atrás na função de finalizar
+
+    
+
+
 
 }
